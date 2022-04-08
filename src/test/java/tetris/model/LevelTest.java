030a511d04caec286e18dfedb264c54ee2e73a0a
biigdata <sokomori@yahoo.fr>
2022-04-08 19:26:44 +0200

    added LevelTest

---
+++ src/test/java/tetris/model/LevelTest.java
@@ -0,0 +1,14 @@
+package tetris.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LevelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
